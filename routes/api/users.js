33283561e1f801905762360e73e7cda4eb4c4526
Joshua Strozzi <strozzij@oregonstate.edu>
2020-10-08 12:25:45 -0700

    async functions replace previous promise based implementation

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,40 +10,54 @@ const { json } = require('express');
 // @desc    Get all users
 // @access  Public
 router.get('/', async (req, res) =>{
-    /*User.find()
-        .sort({date: -1})
-        .then(users => res.json(users))*/
+    
     
     try {
-        const userPage = await getUsers();
-        console.log("user page: ", userPage)
-        res.status(200).send(userPage);
-    } catch (err) {
-        console.log("-- error: ", err);
-        res.status(500).send({
-            error: "Error Fetching users page."
-        });
+        const users = await User.find();
+        if (!users) throw Error('No items');
+    
+        res.status(200).json(users);
+    } catch (e) {
+        res.status(400).json({ msg: e.message });
     }
 });
 
-async function getUsers(){
-    const users = await User.find()
-        .sort({date: -1});
-
-    return users;
-}
 
 // @route   POST /users
 // @desc    Create a User
 // @access  Public
-router.post('/', (req, res) =>{
+router.post('/', async (req, res) =>{
+    if(!req.body.username && !req.body.password) res.status(500).json({message: "Need usrnm & pswd"})
+    
     const newUser = new User({
         username: req.body.username,
         password: req.body.password
     });
     
+    try {
+        const insertUser = await newUser.save();
+        if(!insertUser) throw Error('Something went wrong saving a user');
 
-    newUser.save()
+        try{
+            const newList = new List({
+                userId: newUser._id,
+                list: []
+            });
+
+            const insertList = await newList.save();
+            if(!insertList) throw Error('Something went wrong adding list to user')
+
+            res.status(200).json({
+                user: insertUser,
+                list: insertList
+            })
+        } catch (err) {
+            res.status(400).json({ msg: err.message });
+        }
+    } catch (err) {
+        res.status(400).json({ msg: err.message });
+    }
+    /*newUser.save()
         .then(user => res.json(user));
 
     const newList = new List({
@@ -52,7 +66,7 @@ router.post('/', (req, res) =>{
     });
 
     newList.save().catch(err => console.log(err));
-    console.log("== New User, List: ", newUser, newList)
+    console.log("== New User, List: ", newUser, newList)*/
 });
 
 // @route   DELETE /users/:id
